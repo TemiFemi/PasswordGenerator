e27d35f0d0a6d3b907ef5aa1504353621d8e0e08
Temitayo Jimoh <temi.boy@MacBook-Pro-3.local>
2021-04-06 23:02:10 -0700

    Created functions for each variable

--- script.js
+++ script.js
@@ -5,36 +5,17 @@ var upperCase = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 
 var lowerCase = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
-var number = "1 2 3 4 5 6 7 8 9 10";
+var number = "12345678910";
 
 var specChar = "! @ # $ % ^ & * ( ) _ - = +";
 
-generateBtn = document.querySelector("#generate");
+var passwordLength = 0;
 
-generateBtn.addEventListener("click", function () {
-    var passwordLength = 0;
-    var minLength = 128;
-    while (passwordLength >= 8 || passwordLength <= 128) {
-        var passwordPrompt = prompt("Please enter the amount of chracters that you will like in your password.Your password must be at leat 8 Characters long and no longer than 128 characters");
-
-    }
-    console.log(passwordLength)
-    var caseUpper = false;
-    var caseLower = false;
-    var num = false;
-    var charSpec = false;
-    while (!caseUpper && !caseLower && !num && !charSpec) {
-        var caseUpper = prompt("Would you like uppercase characters in your password?")
-        var caseLower = prompt("Would you like lowercase characters in your password? ")
-        var num = prompt("Would you like number characters in your password?")
-        var charSpec = promt("Would you like special characters in your password?")
-    }
-}
-)
+var lowercasePrompt = "";
 
 
+var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -43,5 +24,74 @@ function writePassword() {
 
 }
 
-  // // Add event listener to generate button
-  // generateBtn.addEventListener("click", writePassword)
+generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+    lengthPassword();
+    lowercaseFunction();
+    uppercaseFunction();
+    specCharFunction();
+    numFunction();
+}
+function lengthPassword() {
+
+    passwordLength = window.prompt("Please enter the amount of chracters that you will like in your password.Your password must be at leat 8 Characters long and no longer than 128 characters");
+
+
+    if (!passwordLength) {
+        return;
+    };
+    if (passwordLength === " " || isNaN(passwordLength) || passwordLength < 9 || passwordLength > 129) {
+        alert("The password length is not acceptable, please try again");
+    };
+}
+
+function lowercaseFunction() {
+    lowercasePrompt = prompt("Would you like your password to include lowercase letter?")
+    lowercasePrompt = lowercasePrompt.toUpperCase();
+
+    if (!lowercasePrompt) {
+        return;
+    }
+    if (lowercasePrompt !== "Yes" && lowercasePrompt !== "No") {
+        alert("Please enter Yes or No")
+    }
+}
+
+function uppercaseFunction() {
+    uppercasePrompt = prompt("Would you like your password to include uppercase letter?")
+
+    if (!uppercasePrompt) {
+        return;
+    };
+
+    if (uppercasePrompt !== "Yes" && uppercasePrompt !== "No") {
+        alert("Please enter Yes or No")
+
+    }
+}
+
+function specCharFunction() {
+    specialCharPrompt = prompt("Would you like your password to include special characters?")
+
+    if (!specialCharPrompt) {
+        return;
+    }
+
+    if (specialCharPrompt !== "Yes" && specialCharPrompt !== "No") {
+        alert("Please enter Yes or No")
+    }
+}
+
+function numFunction() {
+    numberPrompt = prompt("Would you like your password to include numbers?")
+
+    if (!numberPrompt) {
+        return;
+    }
+
+    if (numberPrompt !== "Yes" && numberPrompt !== "No") {
+        alert("Please enter Yes or No")
+
+    }
+}
+
